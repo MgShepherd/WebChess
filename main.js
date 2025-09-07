bde8f6d5d33e8f40a367ba9c5eae320bbe92522f
MgShepherd <michaelshep1504@gmail.com>
2025-09-07 21:03:48 +0100

    Created inital board and adding styling

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 // @ts-check
 
-function pressButton() {
-	console.log("The button has been pressed");
+const BOARD_DIMENSION = 8;
+const BOARD_SIZE = BOARD_DIMENSION * BOARD_DIMENSION;
+
+/**
+ * @enum {string}
+ */
+const Piece = {
+	PAWN: "pawn",
+	ROOK: "rook",
+	KNIGHT: "knight",
+	BISHOP: "bishop",
+	QUEEN: "queen",
+	KING: "king"
+};
+
+/** @type {(Piece|null)[]} */
+const board = Array(BOARD_SIZE).fill(null);
+
+/**
+ * Creates and renders the board
+ */
+function createBoard() {
+	const boardElement = document.getElementById("board");
+
+	if (boardElement != null) {
+		for (let row = 0; row < BOARD_DIMENSION; row++) {
+			for (let col = 0; col < BOARD_DIMENSION; col++) {
+				const square = document.createElement("div");
+				square.classList.add('boardSquare');
+				const isWhiteSquare = (row + col) % 2 == 0;
+				square.style.backgroundColor = isWhiteSquare ? 'white' : 'black';
+				boardElement.appendChild(square);
+			}
+		}
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const myButton = document.getElementById("testButton");
-	if (myButton) {
-		myButton.addEventListener('click', pressButton);
-	}
+	createBoard();
 });
