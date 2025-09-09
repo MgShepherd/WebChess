0906d935059f9c118a2e5200a72f05e399e16b54
MgShepherd <michaelshep1504@gmail.com>
2025-09-09 08:50:58 +0100

    Added event listeners to squares and made it so you can select a square

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 const BOARD_DIMENSION = 8;
-const BOARD_SIZE = BOARD_DIMENSION * BOARD_DIMENSION;
 
 /**
  * @enum {string}
@@ -15,8 +14,62 @@ const Piece = {
 	KING: "king"
 };
 
-/** @type {(Piece|null)[]} */
-const board = Array(BOARD_SIZE).fill(null);
+/**
+ * @typedef {Object} BoardSquare
+ * @property {Piece|null} piece
+ * @property {boolean} active
+ * @property {string} color
+ */
+
+/**
+ * Creates a new instance of the BoardSquare type
+ * @param {string} color - The color of the current square
+ * @returns {BoardSquare} - The new BoardSquare instance
+ */
+function createBoardSquare(color) {
+	return {
+		piece: null,
+		active: false,
+		color
+	};
+}
+
+/** @type {(BoardSquare)[][]} */
+let board = Array.from({ length: BOARD_DIMENSION }, () => Array(BOARD_DIMENSION));
+/** @type {number[]} */
+let activeIdxs = [];
+
+/**
+ * Clears all the previous active squares
+ */
+function clearPreviousActive() {
+	activeIdxs.forEach((idx) => {
+		let row = Math.floor(idx / BOARD_DIMENSION);
+		let col = idx - (row * BOARD_DIMENSION);
+
+		board[row][col].active = false;
+		let boardSquare = document.getElementById(`Square${row}${col}`);
+		if (boardSquare != null) {
+			boardSquare.style.backgroundColor = board[row][col].color;
+		}
+	})
+	activeIdxs = [];
+}
+
+/**
+ * Performs required updates when one of the board squares has been clicked
+ * @param {EventTarget} target - The event where the 
+ * @param {number} row - The row of the square that was clicked
+ * @param {number} col - The col of the square that was clicked
+ */
+function handleSquareClicked(target, row, col) {
+	clearPreviousActive();
+	board[row][col].active = true;
+	activeIdxs.push(row * BOARD_DIMENSION + col);
+	if (target instanceof HTMLElement) {
+		target.style.backgroundColor = 'red';
+	}
+}
 
 /**
  * Creates and renders the board
@@ -29,8 +82,15 @@ function createBoard() {
 			for (let col = 0; col < BOARD_DIMENSION; col++) {
 				const square = document.createElement("div");
 				square.classList.add('boardSquare');
-				const isWhiteSquare = (row + col) % 2 == 0;
-				square.style.backgroundColor = isWhiteSquare ? 'white' : 'black';
+				square.id = `Square${row}${col}`;
+				square.addEventListener("click", (event) => {
+					if (event.target != null) {
+						handleSquareClicked(event.target, row, col)
+					}
+				});
+				const color = (row + col) % 2 == 0 ? 'white' : 'black';
+				board[row][col] = createBoardSquare(color);
+				square.style.backgroundColor = color;
 				boardElement.appendChild(square);
 			}
 		}
